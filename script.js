f6bcb71729140ed2dd832bbd19af3aef41e81aad
K Venkat Sai Chandra <146515779+KVSchandra@users.noreply.github.com>
2023-10-15 03:18:02 +0530

    script.js

--- script.js
+++ script.js
@@ -44,6 +44,6 @@ function updateBigCup() {
         remained.style.height = 0
     } else {
         remained.style.visibility = 'visible'
-        liters.innerText = `${8 - (1 * fullCups )}L`
+        liters.innerText = `${2 - (1 * fullCups )}L`
     }
 }
